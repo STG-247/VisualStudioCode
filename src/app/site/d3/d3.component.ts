09afbb25b14970ad07082843f2d6d6d2eababca4
Shashwat Kaundinya <shashwat_247@yahoo.com>
2019-08-24 18:10:46 +0530

    Switched to SCSS styling from CSS, added favicon.

--- src/app/site/d3/d3.component.ts
+++ src/app/site/d3/d3.component.ts
@@ -4,7 +4,7 @@ import * as d3 from 'd3';
 @Component({
   selector: 'app-d3',
   templateUrl: './d3.component.html',
-  styleUrls: ['./d3.component.css']
+  styleUrls: ['./d3.component.scss']
 })
 export class D3Component implements OnInit, AfterContentInit, AfterViewInit {
 
