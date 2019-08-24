d2dc986d9cb77065d81b91b358f0ae8c23a4b17f
Shashwat Kaundinya <shashwat_247@yahoo.com>
2019-08-25 00:43:27 +0530

    Adding initial layout architecture to the project.

---
+++ src/app/site/layout/off-sidebar/off-sidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-off-sidebar',
+  templateUrl: './off-sidebar.component.html',
+  styleUrls: ['./off-sidebar.component.scss']
+})
+export class OffSidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/site/site.module.ts
+++ src/app/site/site.module.ts
@@ -1,9 +1,14 @@
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
 import { SiteComponent } from './site.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { D3Component } from './d3/d3.component';
 import { SiteRoutingModule } from './site.routing.module';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { OffSidebarComponent } from './layout/off-sidebar/off-sidebar.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports: [
@@ -13,7 +18,12 @@ import { SiteRoutingModule } from './site.routing.module';
   declarations: [
     SiteComponent,
     HomeComponent,
-    D3Component
+    D3Component,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    OffSidebarComponent,
+    LoginComponent
   ]
 })
 export class SiteModule { }
